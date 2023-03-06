1db20c3561031e6053301eb70ee4b8c7cfb11a57
DhanushM12 <dhanushmukhi@gmail.com>
2023-03-06 18:43:53 +0530

    Added hoc

---
+++ src/CounterRender.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default class CounterRender extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {count: 0}
+    }
+    update = type => {
+        if(type === "INC"){
+            this.setState(({count}) => ({count: count+1}));
+        }
+        else if(type === "DEC"){
+            this.setState(({count}) => ({count: count-1}));
+        }
+    }
+  render() {
+    return this.props.render({
+        ...this.state,
+        ...this.props,
+        update: this.update
+    })
+  }
+}
